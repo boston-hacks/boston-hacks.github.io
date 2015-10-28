d7b791f6616b58e384059dcc4778f8ae4b0732c1
Daniel Gorelick <dqgorelick@gmail.com>
2015-10-28 15:50:21 -0400

    [DG]Finished hamburger menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+var collapsed = true;
+function $(query){
+	return document.querySelectorAll(query);
+}
+
+function collapse(){
+	var el = $(".collapse")[0]
+	if (collapsed){
+		el.className += " active"
+		collapsed = false;
+	} else {
+		el.className = "collapse"
+		collapsed = true;
+	}
+
+}
