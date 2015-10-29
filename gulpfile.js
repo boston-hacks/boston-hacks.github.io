9bed7caf119710650bbd5f7561039c8d4df1e97d
Daniel Gorelick <dqgorelick@gmail.com>
2015-10-28 22:02:07 -0400

    [DG] added html boilerplate for new pages

--- gulpfile.js
+++ gulpfile.js
@@ -29,21 +29,21 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('scripts', function() {
-	gulp.src([
-		// paths.bower + '/jquery/dist/jquery.js',
-		// paths.bower + '/foundation/js/foundation.js',
-		paths.assets + 'scripts/app.js'
-	])
-	.pipe(concat('app.js'))
-	.pipe(gulp.dest('./public/js'));
+// gulp.task('scripts', function() {
+// 	gulp.src([
+// 		// paths.bower + '/jquery/dist/jquery.js',
+// 		// paths.bower + '/foundation/js/foundation.js',
+// 		paths.assets + 'scripts/app.js'
+// 	])
+// 	.pipe(concat('app.js'))
+// 	.pipe(gulp.dest('./public/js'));
 
-	return gulp.src(paths.bower + '/modernizr/modernizr.js')
-		.pipe(gulp.dest('./public/js'));
-});
+// 	return gulp.src(paths.bower + '/modernizr/modernizr.js')
+// 		.pipe(gulp.dest('./public/js'));
+// });
 
 gulp.task('watch', function() {
 	gulp.watch(paths.assets + 'styles/**/*.scss', ['styles']);
 	gulp.watch(paths.assets + 'scripts/**/*.js', ['scripts']);
 });
-gulp.task('default', ['watch', 'jshint', 'styles', 'scripts']);
+gulp.task('default', ['watch', 'jshint', 'styles']);
--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,15 @@ function collapse(){
 	var button = $(".collapse-button")[0];
 	if (collapsed){
 		el.className += " active"
-		button.innerHTML = "&#x2716;"
+		button.innerHTML = "&#x2716";
+		button.style.color = "#E51F63";
+		// button.style.font-size = 30;
 		collapsed = false;
 	} else {
 		el.className = "collapse"
 		button.innerHTML = "&#9776;"
+		button.style.color = "#E51F63";
+		// button.style.font-size = 30;
 		collapsed = true;
 	}
 }
