edb2c125343cac81a5398b8ba5f5df634c28fb13
Daniel Gorelick <dqgorelick@gmail.com>
2015-10-28 23:57:47 -0400

    [DG]FAQ page is ready

--- public/js/app.js
+++ public/js/app.js
@@ -11,14 +11,19 @@ function collapse(){
 	if (collapsed){
 		el.className += " active"
 		button.innerHTML = "&#x2716";
-		button.style.color = "#E51F63";
-		// button.style.font-size = 30;
 		collapsed = false;
 	} else {
 		el.className = "collapse"
 		button.innerHTML = "&#9776;"
-		button.style.color = "#E51F63";
-		// button.style.font-size = 30;
 		collapsed = true;
 	}
+}
+
+function faq(selected){
+	var els = $(".faq-question");
+	for (var i = 0; i <= els.length - 1; i++) {
+		els[i].className = "faq-question";
+	};
+	selected.className += " active";
+	console.log(selected);
 }
